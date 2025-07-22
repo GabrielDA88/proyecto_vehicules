d1b0daca7c6010ee885c1c47405348c5068bfefe
Gabriel Na <muskahawck@gmail.com>
2025-07-21 22:37:03 -0600

    Se agrego cabecera a la app

--- app_vehicles.py
+++ app_vehicles.py
@@ -1,17 +1,41 @@
-import pandas as pd
+# Importación de librerias
+
+import pandas as pd 
 import plotly.express as px
-import streamlit as stm
-     
-car_data = pd.read_csv('vehicles_us.csv') # leer los datos
-hist_button = st.button('Construir histograma') # crear un botón
-     
-if hist_button: # al hacer clic en el botón
-         # escribir un mensaje
-         st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+import streamlit as stm 
+
+# Colocación de cabecera
+
+stm.header('Análisis de venta de vehiculos usados')
+
+# Carga o lectura de la información
+
+car_data = pd.read_csv('vehicles_us.csv') 
+
+# Creación de botón
+
+hist_button = stm.button('Construir histograma') # crear un botón
+
+# Asignación de acción al botón
+
+if hist_button:
+    # escribir un mensaje
+    stm.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
          
-         # crear un histograma
-         fig = px.histogram(car_data, x="odometer")
+    # creación de histograma
+    fig = px.histogram(car_data, x="odometer")
      
-         # mostrar un gráfico Plotly interactivo
-         st.plotly_chart(fig, use_container_width=True)
+    # mostrar un gráfico Plotly interactivo
+    stm.plotly_chart(fig, use_container_width=True)
+
+build_scatter = stm.checkbox('Construye un gráfico de dispersión')
+
+if build_scatter:
+    # escribir un mensaje
+    stm.write('Creación de dispersión para el conjunto de datos')
+         
+    # creación de histograma
+    fig = px.scatter(car_data, x="model_year", y = 'price')
      
+    # mostrar un gráfico Plotly interactivo
+    stm.plotly_chart(fig, use_container_width=True)  
